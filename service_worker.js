caf544b8432b690eb3231a8d7776c1ea364cb41a
CoolMasterB <76643837+CoolMasterB@users.noreply.github.com>
2024-02-24 23:12:38 +0530

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,32 +1,9 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-const host = "https://old.reddit.com"
-const regex = /^https?:\/\/(?:www.reddit.com|reddit.com)([\S\s]*)/
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
+  const msg = `Navigation to ${e.request.url} redirected on tab ${e.request.tabId}.`;
+  console.log(msg);
+});
 
-chrome.declarativeNetRequest.addListener(
-  function(details){
-    return {redirectUrl: host + details.url.match(regex)[1]}
-  },
-  {
-    urls: [
-      "*://www.reddit.com/*",
-      "*://reddit.com/*"
-    ]
-  },
-  ["blocking"]
-);
+console.log('Service worker started.');
 
