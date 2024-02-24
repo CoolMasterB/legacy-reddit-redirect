afcd53918fd994c059cdd12e00f4e7eb451595d9
CoolMasterB <76643837+CoolMasterB@users.noreply.github.com>
2024-02-24 23:18:09 +0530

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,16 @@
-'use strict';
+const host = "https://new.reddit.com"
+const regex = /^https?:\/\/(?:www.reddit.com|reddit.com)([\S\s]*)/
 
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
-  const msg = `Navigation to ${e.request.url} redirected on tab ${e.request.tabId}.`;
-  console.log(msg);
-});
-
-console.log('Service worker started.');
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details){
+    return {redirectUrl: host + details.url.match(regex)[1]}
+  },
+  {
+    urls: [
+      "*://www.reddit.com/*",
+      "*://reddit.com/*"
+    ]
+  },
+  ["blocking"]
+);
 
