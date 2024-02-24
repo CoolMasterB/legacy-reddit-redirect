8e7bc23787c34f3eabaaed2d4f4bb032b2e292ff
CoolMasterB <76643837+CoolMasterB@users.noreply.github.com>
2024-02-24 21:30:07 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
-const host = "https://new.reddit.com"
-const regex = /^https?:\/\/(?:www.reddit.com|reddit.com)([\S\s]*)/
+function redirect(reddit) {
+    let url = "https://new.reddit.com/" + reddit;
+    chrome.tabs.update({ url: url });
+}
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details){
-    return {redirectUrl: host + details.url.match(regex)[1]}
-  },
-  {
-    urls: [
-      "*://www.reddit.com/*",
-      "*://reddit.com/*"
-    ]
-  },
-  ["blocking"]
-);
+// Add the listener when the extension is installed or started
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.omnibox.setDefaultSuggestion({ description: "Redirect to new.reddit.com" });
+});
+
+// Add the listener for omnibox input
+chrome.omnibox.onInputEntered.addListener(function (text) {
+    redirect(text);
+});
