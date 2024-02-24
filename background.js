fe03fd5452bb47522bbe2dc60fc5615e5292c6b5
CoolMasterB <76643837+CoolMasterB@users.noreply.github.com>
2024-02-24 21:44:40 +0530

    Update background.js

--- background.js
+++ background.js
@@ -4,9 +4,6 @@ function redirect(reddit) {
     chrome.tabs.update({ url: url });
 }
 
-// Set a default suggestion for the omnibox
-chrome.omnibox.setDefaultSuggestion({ description: "Redirect to new.reddit.com" });
-
 // Add the listener for omnibox input
 chrome.omnibox.onInputEntered.addListener(function (text) {
     redirect(text);
