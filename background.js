d7f57e604c9784652893c053b1f8e22b31dbe349
CoolMasterB <76643837+CoolMasterB@users.noreply.github.com>
2024-02-24 21:37:11 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
+// background.js
+
+// Function to redirect reddit.com pages to new.reddit.com
 function redirect(reddit) {
     let url = "https://new.reddit.com/" + reddit;
     chrome.tabs.update({ url: url });
 }
 
-// Add the listener when the extension is installed or started
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.omnibox.setDefaultSuggestion({ description: "Redirect to new.reddit.com" });
-});
+// Set a default suggestion for the omnibox
+chrome.omnibox.setDefaultSuggestion({ description: "Redirect to new.reddit.com" });
 
 // Add the listener for omnibox input
 chrome.omnibox.onInputEntered.addListener(function (text) {
     redirect(text);
 });
+
