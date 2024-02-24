889d0c79458cba60db9c36777c62f4edfaed1006
CoolMasterB <76643837+CoolMasterB@users.noreply.github.com>
2024-02-24 21:49:15 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,21 @@
-// Function to redirect reddit.com pages to new.reddit.com
-function redirect(reddit) {
-    let url = "https://new.reddit.com/" + reddit;
-    chrome.tabs.update({ url: url });
-}
+const host = "https://new.reddit.com";
+const regex = /^https?:\/\/(?:www.reddit.com|reddit.com)([\S\s]*)/;
 
-// Add the listener for omnibox input
-chrome.omnibox.onInputEntered.addListener(function (text) {
-    redirect(text);
+chrome.declarativeNetRequest.updateDynamicRules({
+  addRules: [{
+    id: "redirectRule",
+    priority: 1,
+    action: {
+      type: "redirect",
+      redirect: {
+        url: host + "$1"
+      }
+    },
+    condition: {
+      regexFilter: regex.source,
+      resourceTypes: ["main_frame", "sub_frame"]
+    }
+  }]
 });
+
 
